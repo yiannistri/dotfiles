007e756bf4b3eb89d46e583b093eebe53dacf157
Yiannis <yiannis.triantafyllopoulos@gmail.com>
2020-05-26 14:21:14 +0100

    Fixed osx script

--- osx-install.sh
+++ osx-install.sh
@@ -22,6 +22,10 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 fi
 
+## Install zsh-autosuggestions
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
 ## Install dotfiles
 for file in $(find $PWD -type f -maxdepth 1 -name ".*" -not -name ".git"); do
     filename=$(basename $file);
@@ -51,4 +55,7 @@ git clone https://github.com/ryanoasis/vim-devicons ~/.vim/bundle/vim-devicons
 brew upgrade
 brew cleanup
 
-./code.sh
+## VSCode extensions
+code --install-extension timonwong.shellcheck
+code --install-extension ms-vscode.go
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
